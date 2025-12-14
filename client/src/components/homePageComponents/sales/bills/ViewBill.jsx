b6ffeed04554384f70d528f13cbf3b7b86b091b1
Abdul Basit <ab9133711@gmail.com>
2025-12-14 13:09:28 +0500

    Change shop name in barcode viewbill and quotation list

--- client/src/components/homePageComponents/sales/bills/ViewBill.jsx
+++ client/src/components/homePageComponents/sales/bills/ViewBill.jsx
@@ -31,8 +31,7 @@ const ViewBill = React.forwardRef((props, ref) => {
                         <div className='flex pl-4'>
                             
                             <div className='flex items-end justify-center '>
-                                <span className='text-4xl font-extrabold pb-2 pr-2'>PARKO </span> <h2 className="text-xl font-bold pb-2"> ELECTRIC AND ELECTRONICS</h2>
-                                <span className='text-xs pb-6 pl-2'>RTM 294967</span>
+                                <span className='text-4xl font-extrabold pb-2 pr-2'>Hilal </span> <h2 className="text-xl font-bold pb-2"> Shopping Mall And Bakers</h2>
                             </div>
                         </div>
                         <p className="text-sm ">{bill?.storeAddress}</p>
